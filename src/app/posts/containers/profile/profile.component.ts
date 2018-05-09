52eab21cc85427329ea370afde70ac6907ff5317
Sam <zhao.shengjun@gmail.com>
2018-05-09 14:16:10 +1000

    update profile component to use router parameters

--- src/app/posts/containers/profile/profile.component.ts
+++ src/app/posts/containers/profile/profile.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"]
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  public profile = { id: null };
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.params.subscribe(
+      res => (this.profile.id = `prifleId = ${res["profileId"]}`)
+    );
   }
-
 }
