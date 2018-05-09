de96209294f8e6a98acf08c64cfbcea26b7f8e31
Sam <zhao.shengjun@gmail.com>
2018-05-09 13:17:57 +1000

    update header component

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './components/layout/layout.component';
+import { RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LayoutComponent } from "./components/layout/layout.component";
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [LayoutComponent]
+  imports: [CommonModule, RouterModule],
+  declarations: [LayoutComponent, HeaderComponent]
 })
-export class UiModule { }
+export class UiModule {}
