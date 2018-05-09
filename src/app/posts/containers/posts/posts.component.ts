431c0b76e678ca11ca9f170ab66bdace5c78e5c7
Sam <zhao.shengjun@gmail.com>
2018-05-09 14:03:37 +1000

    add dummy data for posts

--- src/app/posts/containers/posts/posts.component.ts
+++ src/app/posts/containers/posts/posts.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  selector: "app-posts",
+  templateUrl: "./posts.component.html",
+  styleUrls: ["./posts.component.css"]
 })
 export class PostsComponent implements OnInit {
+  public posts = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    for (let i = 1; i < 10; i++) {
+      this.posts.push({
+        id: i,
+        text: "This is post with id " + i
+      });
+    }
   }
-
 }
