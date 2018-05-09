5a34ebc034a2b26a8ab4a3c77492a76b67fe8d7b
Sam <zhao.shengjun@gmail.com>
2018-05-09 13:58:25 +1000

    add Profile component

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,8 +1,12 @@
+import { ProfileComponent } from "./containers/profile/profile.component";
 import { PostsComponent } from "./containers/posts/posts.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [{ path: "", component: PostsComponent }];
+const routes: Routes = [
+  { path: "", component: PostsComponent },
+  { path: ":profileId", component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './containers/posts/posts.component';
+import { ProfileComponent } from './containers/profile/profile.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PostsRoutingModule
   ],
-  declarations: [PostsComponent]
+  declarations: [PostsComponent, ProfileComponent]
 })
 export class PostsModule { }
